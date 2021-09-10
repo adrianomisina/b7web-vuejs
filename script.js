675c63d039bd02b2f482f3b915b4dc4a309e0576
Adriano Misina <adriano.misina@gmail.com>
2021-09-10 12:36:32 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// let placar = new Vue ({
+//     el: '#placar',
+//     data: {
+//         n: 0,
+//         aparecer: true
+//     }
+// });
+
+let lista = new Vue({
+    el:'#lista',
+    data: {
+        nomes: [
+            {nome:'Adriano', sobrenome: 'Misina', idade: 39},
+            {nome:'Kenzo', sobrenome: 'Takeshi', idade: 04},
+            {nome:'Jane', sobrenome: 'Costa', idade: 33},
+            {nome:'Melissa', sobrenome: 'Docinho', idade: 08},
+        ]
+    }
+})
